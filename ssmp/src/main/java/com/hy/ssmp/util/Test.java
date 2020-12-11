a91a7607b88743ece62acd7434d578445256d902
liuaiying <1652173894@qq.com>
2020-12-11 11:03:31 +0800

    新增Test嘻嘻嘻嘻

--- ssmp/src/main/java/com/hy/ssmp/util/Test.java
+++ ssmp/src/main/java/com/hy/ssmp/util/Test.java
@@ -2,6 +2,6 @@ package com.hy.ssmp.util;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("哈哈哈哈哈哈哈");
+        System.out.println("哈哈哈哈哈哈哈嘻嘻嘻嘻");
     }
 }
