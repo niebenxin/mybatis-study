e07c478eb48554524d874ad52112330da8cfce5b
niebenxin <3106332048@qq.com>
2020-12-11 10:43:31 +0800

    修改成哈哈哈哈哈

--- ssmp/src/main/java/com/hy/ssmp/util/Test.java
+++ ssmp/src/main/java/com/hy/ssmp/util/Test.java
@@ -2,6 +2,6 @@ package com.hy.ssmp.util;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("git test");
+        System.out.println("哈哈哈哈哈哈哈");
     }
 }
